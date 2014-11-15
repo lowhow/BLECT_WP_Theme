71786e5f03c5f5d92feb49bb51838ac8416adb1b
Low How <lowhow@gmail.com>
2014-11-15 22:00:36 +0800

    fix missing files

--- content/themes/blect/functions.php
+++ content/themes/blect/functions.php
@@ -10,43 +10,80 @@
 /**
  * Bootstrapping framework to WordPress theme and initialise.
  */
-require get_template_directory() . '/framework.php';
+require get_template_directory() . '/blect-config.php';
 
-$vh = new VunHougkh( 'WordPress' );
+// $vh = new VunHougkh( 'WordPress' );
+
+// if (BLECT_CMS_DIR){
+// 	echo BLECT_CMS_DIR;
+// }else{
+// 	echo  'BLECT_CMS_DIR is not defined.';
+// }
 
 
 /** 
  * Setup Redux Options 
  */
-if ( ! function_exists( 'setup_redux' ) ){
-	function setup_redux() {
-		if ( !isset( $t_options ) && file_exists( trailingslashit( FW_THEME_ADMIN_DIR ) . 'themeoptions-config.php' ) ) {
-		    require_once( trailingslashit( FW_THEME_ADMIN_DIR ) . '/themeoptions-config.php' );
-		}
-	}
+// if ( ! function_exists( 'setup_redux' ) ){
+// 	function setup_redux() {
+// 		if ( !isset( $t_options ) && file_exists( trailingslashit( FW_THEME_ADMIN_DIR ) . 'themeoptions-config.php' ) ) {
+// 		    require_once( trailingslashit( FW_THEME_ADMIN_DIR ) . '/themeoptions-config.php' );
+// 		}
+// 	}
 
-	setup_redux();
-}
+// 	setup_redux();
+// }
+
+
+//require get_template_directory() . '/functions/sample-function-file.php';
+
 
 
 /**
-	Load customisable theme function files. 
- *
- * Must always load theme.php first. Append subsequent function files if any.
+
++-+-+-+-+-+-+-+-+-+-+-+
+|p|r|e|t|t|y|P|h|o|t|o|
++-+-+-+-+-+-+-+-+-+-+-+
+ 
  */
-require get_template_directory() . '/functions/theme.php';
-require get_template_directory() . '/functions/t_helper.php';
+
+if ( is_admin() ) {
+	add_filter( 'image_send_to_editor', 'fw_add_prettyPhoto_rel_in_editor', 10, 7 );
+	add_filter( 'wp_get_attachment_link', 'fw_add_prettyPhoto_rel_in_gallery', 10, 4 );
+}
+
+/** 
+ *  Hook into 'image_send_to_editor' and place rel="prettyPhoto" 
+ */
+function fw_add_prettyPhoto_rel_in_editor($html, $id, $alt, $title, $align, $url, $size ) {
+  global $post;
+  global $permalink;
+
+  $hook = "rel";
+  $selector = 'prettyPhoto';
+  
+  if ( ! $permalink )
+    $html = preg_match( '/' . $hook . '="/', $html ) ? str_replace( $hook . '="', $hook . '="' . $selector. '" ' , $html ) : str_replace( '<a ', '<a ' . $hook . '="' . $selector . '" ', $html );
+  
+  return $html;
+}
 
 
 /** 
- * Setup Redux Demo Options. Remove after development 
+ * Hook into 'wp_get_attachment_link' and place rel="prettyPhoto" 
  */
-/*if ( !isset( $redux_demo ) && file_exists( trailingslashit( FW_LIB_DIR ) . 'ReduxFramework/sample/sample-config.php' ) ) {
-    require_once( trailingslashit( FW_LIB_DIR ) . 'ReduxFramework/sample/sample-config.php' );
-}*/
+function fw_add_prettyPhoto_rel_in_gallery($html, $id, $size, $permalink) {
+	global $post;
 
-/** example */
-//require get_template_directory() . '/functions/sample-function-file.php';
+	$pid = $post->ID;
+
+  $hook = "rel";
+  $selector = 'prettyPhoto';
+
+	if ( ! $permalink )
+		$html = preg_match( '/' . $hook . '="/', $html ) ? str_replace( $hook . '="', $hook . '="' . $selector . '[gallery-' . $pid . '] ', $html ) : str_replace( '<a ', '<a ' . $hook . '="' . $selector . '[gallery-' . $pid . ']" ', $html );
+	return $html;
+}
 
 
 
