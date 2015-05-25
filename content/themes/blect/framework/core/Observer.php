930c08ed6a3ca6a79ad3329afa416460ac6838cd
Low How <lowhow@gmail.com>
2015-05-25 20:04:08 +0800

    replacing Core

--- content/themes/blect/framework/core/Observer.php
+++
@@ -1,6 +0,0 @@
-<?php namespace Framework\Core;
-
-interface Observer
-{
-	public function handle();
-}
--- content/themes/blect/framework/core/Subject.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Framework\Core;
-
-interface Subject
-{
-	public function attach( Observer $observer );
-	public function detach( $index );
-	public function notify();
-}
